75407a6533c68ba3441b8de48131685ce5fcefa1
abishek <m.abiakil@gmail.com>
2020-06-28 09:26:26 +0530

    pythoncode

---
+++ fileNameChange_pyCode/listDirPyCode.py
@@ -0,0 +1,26 @@
+# import os
+
+# root = os.path.join('', 'food')
+# for directory, subdir_list, file_list in os.walk(root):
+#     print('Directory:', directory)
+#     for name in subdir_list:
+#         print('Subdirectory:', name)
+#     for name in file_list:
+#         print('File:', name)
+#     print()
+
+import datetime
+import os
+
+root = os.path.join('', 'food')
+for directory, subdir_list, file_list in os.walk(root):
+    for name in file_list:
+        source_name = os.path.join(directory, name)
+        timestamp = os.path.getmtime(source_name)
+        modified_date = str(datetime.datetime.fromtimestamp(
+            timestamp)).replace(':', '.')
+        target_name = os.path.join(directory, f'{modified_date}_{name}')
+
+        print(f'Renaming: {source_name} to: {target_name}')
+
+        os.rename(source_name, target_name)
